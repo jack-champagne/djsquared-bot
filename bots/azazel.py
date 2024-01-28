3d303c10e004cb2b28b113fbd57abb075c91209d
Jake Zaia <jlz2369@rit.edu>
2024-01-27 21:28:07 -0500

    Azazel mk 2

--- bots/azazel.py
+++ bots/azazel.py
@@ -4,14 +4,18 @@ from src.robot_controller import RobotController
 from src.player import Player
 from src.map import Map
 
-class BotPlayer(Player):
-    IDEAL_HEALTH = 30
+IDEAL_HEALTH = 50
+AFFORDABLE_HEALTH = 24
 
-    def __init__(self, map: Map):
+class BotPlayer(Player):
+
+    def __init__(self, map: Map, desired_debris_health=IDEAL_HEALTH):
         self.map = map
+        self.desired_debris_health = desired_debris_health
 
     def play_turn(self, rc: RobotController):
-        if rc.can_send_debris(1, self.IDEAL_HEALTH):
-            rc.send_debris(1, self.IDEAL_HEALTH)
-        else:
-            rc.send_debris(1, 9)
+        if rc.can_send_debris(1, self.desired_debris_health):
+            rc.send_debris(1, self.desired_debris_health)
+        elif rc.can_send_debris(1, AFFORDABLE_HEALTH):
+            rc.send_debris(1, AFFORDABLE_HEALTH)
+        # otherwise do nothing
